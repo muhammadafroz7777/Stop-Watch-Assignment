ca334ce13a13ef357502b666e1a04f207bf4661f
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-05-05 16:51:04 +0500

    conti

---
+++ app.js
@@ -0,0 +1,20 @@
+var $minutes = document.getElementById("@minutes")
+var $seconds = document.getElementById("@seconds")
+var $milliseconds = document.getElementById("@milliseconds")
+
+
+
+
+
+
+function start() {
+    
+}
+
+function stop() {
+    
+}
+
+function Reset() {
+    
+}
